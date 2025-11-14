a6282d305b5190ef6ba60c952bf2a27deaedbb2a
kupkovski <kupkovski@gmail.com>
2025-11-14 19:48:15 -0300

    separate files

--- main.py
+++ main.py
@@ -1,48 +1,8 @@
-from typing import Protocol
-
-
-class PaymentProcessor(Protocol):
-    def process_payment(self, amount):
-        ...
-
-
-class CreditCardProcessor(PaymentProcessor):
-    def process_payment(self, amount):
-        print(f"Processing credit card payment of ${amount}")
-
-
-class PayPalProcessor(PaymentProcessor):
-    def process_payment(self, amount):
-        print(f"Processing PayPal payment of ${amount}")
-
-
-class PaymentFactory:
-    @staticmethod
-    def create_payment_processor(payment_method):
-        if payment_method == "credit_card":
-            return CreditCardProcessor()
-        elif payment_method == "paypal":
-            return PayPalProcessor()
-        else:
-            raise ValueError("Invalid payment method")
-
-
-class Order:
-    def __init__(self, items, payment_processor):
-        self.items = items
-        self.payment_processor = payment_processor
-        self.total = sum(item.price * item.quantity for item in items)
-
-    def process_payment(self):
-        self.payment_processor.process_payment(self.total)
-        print("Payment processed successfully")
-
-
-class Item:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+from item import Item
+from order import Order
+from payment_factory import PaymentFactory
+from credit_card_processor import CreditCardProcessor
+from paypal_processor import PayPalProcessor
 
 
 if __name__ == "__main__":
@@ -50,7 +10,11 @@ if __name__ == "__main__":
 
     payment_method = "credit_card"
 
-    payment_processor = PaymentFactory.create_payment_processor(payment_method)
+    factory = PaymentFactory()
+    factory.register_processor("credit_card", CreditCardProcessor)
+    factory.register_processor("paypal", PayPalProcessor)
+
+    payment_processor = factory.create_payment_processor(payment_method)
 
     order = Order(items, payment_processor)
 
